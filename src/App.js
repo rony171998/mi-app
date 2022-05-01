a99dadb9ae04418cc4177dec0911069dedf422a5
rony171998 <rony171998@gmail.com>
2022-05-01 09:23:38 -0500

    funcionalida v 0.9

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
+import ChangeColorBackground from './components/ChangeColorBackground';
 import ManagerUser from './components/ManagerUser';
 import MyComponents from './components/MyComponents';
+import QuoteBox from './components/QuoteBox';
 
 function App() {
-
+  
   return (
     <div>
-      
-      <div className="users">
-        <ManagerUser/>
-      </div>
+      <ChangeColorBackground/>
+      <QuoteBox/> 
     </div>
 
   );
